5804298c511ab9210dd780604f94b925d373c3c0
Vincent-hide <hidehiro.okayama@gmail.com>
2020-10-09 17:09:13 -0400

    folders and files restructured

---
+++ resolvers/task.js
@@ -0,0 +1,26 @@
+const {tasks, users} = require('../constants');
+const uuid = require('uuid');
+
+module.exports = {
+  Query: {
+    tasks: () => {
+      console.log(tasks);
+      return tasks;
+    },
+    task: (parent, args) => {
+      console.log("Parent:", parent);
+      return tasks.find(task => task.id === args.id);
+    },
+  },
+  Mutation: {
+    createTask: (_, { input }) => {
+      const task = { ...input, id:uuid.v4() };
+      tasks.push(task);
+      return task;
+    }
+  },
+  Task: {
+    user: (parent) => users.find(user => user.id === parent.userId)
+    // or user: ({ userId }) => users.find(user => user.id === userId)
+  },
+};
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const {ApolloServer, gql} = require('apollo-server-express');
 const cors = require('cors');
+
+
 require('dotenv').config();
 
-const {tasks, users} = require('./constants');
-const uuid = require('uuid');
+const resolvers = require('./resolvers');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -45,36 +46,6 @@ const typeDefs = gql`
         createTask(input: createTaskInput): Task
     }
 `
-
-const resolvers = {
-  Query: {
-    greetings: () => null,
-    tasks: () => {
-      console.log(tasks);
-      return tasks;
-    },
-    task: (parent, args) => {
-      console.log("Parent:", parent);
-      return tasks.find(task => task.id === args.id);
-    },
-    users: () => users,
-    user: (_, {id}) => users.find(user => user.id === id),
-  },
-  Mutation: {
-    createTask: (_, { input }) => {
-      const task = { ...input, id:uuid.v4() };
-      tasks.push(task);
-      return task;
-    }
-  },
-  Task: {
-    user: (parent) => users.find(user => user.id === parent.userId)
-    // or user: ({ userId }) => users.find(user => user.id === userId)
-  },
-  User: {
-    tasks: ({id}) => tasks.filter(task => task.userId === id)
-  }
-};
 
 const apolloServer = new ApolloServer({
   typeDefs,
